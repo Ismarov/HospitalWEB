f90d53b22037fe178a9e3bd29e2866ee1d843976
ricardo <rccursach@gmail.com>
2015-08-12 16:10:00 -0300

    llamadas ajax a crear medico

---
+++ WebContent/js/hospital/d_ingreso_medico.js
@@ -0,0 +1,37 @@
+var url = "ingresarMedico";
+
+$(document).ready(function(){
+
+	$("#btnIngresar").click(function(){
+		if(validar()){
+			$.post(url, $("#medicoForm").serialize())
+			.success(function(data){
+				console.log(data);
+				d = eval("("+data+")");
+				if(d != null){
+					alert("Los datos se ingresaron existosamente");
+				}
+				else{
+					alert("Los datos no pudieron ser ingresados, Espere un momento y vuelva a intentarlo");
+				}
+			})
+			.error(function(data){
+				alert("Ocurrió un error al intentar insertar los datos,\nReintentelo en un momento.");
+			});
+		}
+		else{
+			alert("Debe completar todos los campos");
+		}
+	});
+});
+
+function validar(){
+	var inp = $("#medicoForm input");
+	var ret = true;
+	$.each(inp, function(i, el){
+		if($(el).val() == ""){
+			ret = false;
+		}
+	});
+	return ret;
+}
